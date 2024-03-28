9c1f981fd91befb3c80dad34dc15e87a0c7b263c
경택 박 <rudxorrmrwkd@gmail.com>
2024-03-28 15:52:49 +0900

    [업데이트] 엔진 업데이트

--- DirectX_PKT/MyCore/MyCore.cpp
+++ DirectX_PKT/MyCore/MyCore.cpp
@@ -1,5 +1,6 @@
 #include"PreCompile.h"
 #include "MyCore.h"
+#include"PlayGameMode.h"
 
 UMyCore::UMyCore()
 {
@@ -11,6 +12,9 @@ UMyCore::~UMyCore()
 
 void UMyCore::Initialize()
 {
+	GEngine->CreateLevel<APlayGameMode>("PlayLevel");
+	GEngine->ChangeLevel("PlayLevel");
+
 	// GEngine->CreateLevel();
 	int a = 0;
 }
